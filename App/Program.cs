216e935699dee3e6589db1caa2f82243caba93ff
Wistaster <wistaster@hotmail.com>
2024-03-24 01:37:05 +0300

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        
+        // www
+        // Features...
+        // Request, Res.
+        // AutoMapper, Validators.
+        // Cqrs -> RequestHandlers.
+        // ILog, Cache, Authorizations.
+    }
+}
---
+++ Web.Apixs/Controllers/HiQController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Apixs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HiQController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hi Q,");
+        }
+    }
+}
